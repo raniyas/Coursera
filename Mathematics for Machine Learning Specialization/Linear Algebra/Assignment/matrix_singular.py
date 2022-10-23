67aa4270cf3dce841ada404edae22c1e435017b2
Raniya <raniyashams1@gmail.com>
2022-10-23 14:57:44 +0530

    Update

---
+++ Mathematics for Machine Learning Specialization/Linear Algebra/Assignment/matrix_singular.py
@@ -0,0 +1,69 @@
+# GRADED FUNCTION
+import numpy as np
+
+
+A = np.array([
+        [2, 0, 0, 0],
+        [0, 3, 0, 0],
+        [0, 0, 4, 4],
+        [0, 0, 5, 5]
+    ], dtype=np.float_)
+
+# Our function will go through the matrix replacing each row in order turning it into echelon form.
+# If at any point it fails because it can't put a 1 in the leading diagonal,
+# we will return the value True, otherwise, we will return False.
+# There is no need to edit this function.
+def isSingular(A) :
+    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
+    try:
+        fixRowZero(B)
+        fixRowOne(B)
+      
+    except MatrixIsSingular:
+        return True
+    return False
+
+# This next line defines our error flag. For when things go wrong if the matrix is singular.
+# There is no need to edit this line.
+class MatrixIsSingular(Exception): pass
+
+# For Row Zero, all we require is the first element is equal to 1.
+# We'll divide the row by the value of A[0, 0].
+# This will get us in trouble though if A[0, 0] equals 0, so first we'll test for that,
+# and if this is true, we'll add one of the lower rows to the first one before the division.
+# We'll repeat the test going down each lower row until we can do the division.
+# There is no need to edit this function.
+def fixRowZero(A) :
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[1]
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[2]
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[3]
+    if A[0,0] == 0 :
+        raise MatrixIsSingular()
+    A[0] = A[0] / A[0,0]
+    print(A)
+    return A
+
+# First we'll set the sub-diagonal elements to zero, i.e. A[1,0].
+# Next we want the diagonal element to be equal to one.
+# We'll divide the row by the value of A[1, 1].
+# Again, we need to test if this is zero.
+# If so, we'll add a lower row and repeat setting the sub-diagonal elements to zero.
+# There is no need to edit this function.
+def fixRowOne(A) :
+    A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        A[1] = A[1] + A[2]
+        A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        A[1] = A[1] + A[3]
+        A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        raise MatrixIsSingular()
+    A[1] = A[1] / A[1,1]
+    print(A)
+    return A
+
+isSingular(A)
